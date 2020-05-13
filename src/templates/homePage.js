f07c71e606cc062fff7b7d62ee3526bd86b1867a
Rochdi <roch2di@gmail.com>
2020-05-14 01:30:21 +0200

    Reconfig cms content

--- src/templates/homePage.js
+++ src/templates/homePage.js
@@ -1,7 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
+
+import { useLanguages } from "../utils/utils"
 
 const HomePage = () => {
+	const { lang } = useLanguages()
+	const {allMarkdownRemark} = useStaticQuery(graphql`
+{
+  allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "home-template"}}}) {
+    edges {
+      node {
+        frontmatter {
+          english {
+            title
+            speciality
+            paragraph
+            introduction
+          }
+          french {
+            title
+            speciality
+            paragraph
+            introduction
+          }
+        }
+      }
+    }
+  }
+}
+`)
+
+	// const { frontmatter } = allMarkdownRemark.edges[0].node
+	// const languageFilter = frontmatter.filter(obj => Object.keys(obj).includes(lang))
+
+	// console.log([...frontmatter])
 	return (
 	  <React.Fragment>
 	    <h1>Hi people</h1>
