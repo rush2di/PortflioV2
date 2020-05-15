8a3213f16d81820161f49dff5f1a1411c490eefc
Rochdi <roch2di@gmail.com>
2020-05-15 20:22:04 +0200

    Update home structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,44 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import HomePage from "../templates/homePage"
 
-const IndexPage = () => (
-    <HomePage />
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+{
+  allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "home-template"}}}) {
+    edges {
+      node {
+        frontmatter {
+          english {
+            title
+            speciality
+            paragraph
+            introduction
+            tsheading
+            tsparagraph
+            psheading
+          }
+          french {
+            title
+            speciality
+            paragraph
+            introduction
+            tsheading
+            tsparagraph
+            psheading
+          }
+        }
+      }
+    }
+  }
+}
+`)
+  return (
+    <HomePage data={allMarkdownRemark} />
+    )
+}
+    
+
 
 export default IndexPage
