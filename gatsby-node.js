114baf00c5e73a5d5f0c29e2815c42aa8234f5b8
Rochdi <roch2di@gmail.com>
2020-05-15 20:22:31 +0200

    Set createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,47 @@ const { fmImagesToRelative } = require("gatsby-remark-relative-images")
 const { createFilePath } = require("gatsby-source-filesystem") 
 const path = require("path")
 
+const makeRequest = (graphql, request) => {
+  return new Promise((resolve, reject) => {
+    resolve(
+      graphql(request).then((result) => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+        return result
+      })
+    )
+  })
+}
+
+exports.createPages = ({ graphql, actions: { createPage } }) => {
+	const getProjects = makeRequest(graphql,`
+		{
+		  allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "projects-template"}}}, sort: {fields: [frontmatter___date], order: DESC}) {
+		    edges {
+		      node {
+		        id
+		        fields {
+		          slug
+		        }
+		      }
+		    }
+		  }
+		}
+	 	`).then(res => {
+		const projects = res.data.allMarkdownRemark.edges
+		projects.forEach((edge) => {
+      const { id, fields } = edge.node
+      createPage({
+        path: `/projects${fields.slug}`,
+        component: path.resolve("src/templates/projectsPage.js"),
+        context: { id },
+      })
+    })
+	})
+	return getProjects
+}
+
 exports.onCreateNode = ({ node, actions: { createNodeField }, getNode}) => {
 	fmImagesToRelative(node)
 	if(node.internal.type === "MarkdownRemark") {
