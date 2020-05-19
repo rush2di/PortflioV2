398f40c9c16b55c59954b6941a71b818e3767191
Rochdi <roch2di@gmail.com>
2020-05-19 03:41:59 +0200

    Update home structure

--- src/templates/homePage.js
+++ src/templates/homePage.js
@@ -1,29 +1,59 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 
-import { useLanguages, languageFilter } from "../utils/utils"
+import { useLanguages, languageFilter, useThemes } from "../utils/utils"
 
 const HomePage = ({data}) => {
 	const { lang } = useLanguages()
 	const { frontmatter } = data.edges[0].node
 	const content = languageFilter(frontmatter, lang)
-	const { title, introduction, paragraph, speciality, tsheading, tsparagraph, psheading } = content
+	const { title, introduction, paragraph, speciality, tsheading, tsparagraph, psheading } = content	
+  const { textStyle } = useThemes()
+
+	const titleFixer = () => (
+		<React.Fragment>
+			<span>{title.substring(0, title.search("Ro"))}</span>
+			<br/>
+			<span>{title.substring(title.search("Ro"))}</span>
+		</React.Fragment>
+	)
 
 	return (
-	  <React.Fragment>
-	    <h1>{title}</h1>
-	    <span>{speciality}</span>
-	    <p><span>{introduction}</span>{paragraph}</p>
-	    <div>
-	    	<h3>{tsheading}</h3>
-	    	<p>{tsparagraph}</p>
+	  <div className="container">
+	  	<div className="section-hero-wrapper">
+			  <div className="section-hero">
+			    <h1 className="heading heading-xl">{titleFixer()}</h1>
+			  </div>
+			  <div className="section-hero-grid">
+					<span className={textStyle}>{speciality}</span>
+			    <p><span className={textStyle}>{introduction} </span>{paragraph}</p>
+		    </div>
+	    </div>
+	    <div className="section-skills-wrapper">
+		    <div className="section-skills-grid">
+			    <div className="section-skills-box">
+			    	<h3 className="heading heading-sm">{tsheading}</h3>
+			    	<p>{tsparagraph}</p>	    	
+			    </div>
+			    <div className="section-skills-box">
+			    	<div className="box box-pf" />
+			    	<div className="box box-pf" />
+			    	<div className="box box-pf" />
+			    	<div className="box box-pm" />
+			    	<div className="box box-pm" />
+			    	<div className="box box-pm" />
+			    	<div className="box box-pl" />
+			    	<div className="box box-pl" />
+			    	<div className="box box-pl" />
+			    </div>
+		    </div>
 	    </div>	    
 	    <div>
 	    	<h3>{psheading}</h3>
 	    	<ProjectsSection lang={lang}/>
 	    </div>
 	    <Link to="/page-2/">Go to page 2</Link>
-	  </React.Fragment>
+	  </div>
 	)
 }
 
