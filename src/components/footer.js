796ba759a1ed54f11cb751da69201872edc5b5b2
rush2di <roch2di@gmail.com>
2020-05-28 18:55:55 +0000

    Update styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,76 +10,78 @@ import linkedinIcon from "../assets/linkedin.png"
 import Formy from "./form"
 
 const Footer = () => {
-	const { lang } = useLanguages()
- 	const { allMarkdownRemark } = useStaticQuery(graphql`
- 	{
-  allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "personal-information"}}}) {
-    edges {
-      node {
-        frontmatter {
-        	github
-        	linkedin
-        	email
-        	phone
-          english {
-            aboutHead
-            aboutText
-            contactHead
-          }
-          french {
-            aboutHead
-            aboutText
-            contactHead
+  const { lang } = useLanguages()
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(
+        filter: { frontmatter: { templateKey: { eq: "personal-information" } } }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              github
+              linkedin
+              email
+              phone
+              english {
+                aboutHead
+                aboutText
+                contactHead
+              }
+              french {
+                aboutHead
+                aboutText
+                contactHead
+              }
+            }
           }
         }
       }
     }
-  }
-}
-`)
- 	
- 	const { frontmatter } = allMarkdownRemark.edges[0].node
-	const content = languageFilter(frontmatter, lang)
+  `)
 
-	const { aboutHead, aboutText, contactHead } = content
-	const { github, linkedin, phone } = frontmatter
+  const { frontmatter } = allMarkdownRemark.edges[0].node
+  const content = languageFilter(frontmatter, lang)
 
-	return (
-		<footer>
-      <div 
-        style={{backgroundImage: `url(${footerHead})`}}
+  const { aboutHead, aboutText, contactHead } = content
+  const { github, linkedin, phone } = frontmatter
+
+  return (
+    <footer>
+      <div
+        style={{ backgroundImage: `url(${footerHead})` }}
         className="footer-head"
-       />
+      />
       <div className="footer-wrapper">
         <div className="footer-body container">
-           <div className="footer-body-grid">
-             <div className="footer-box">
-                <h3 className="heading heading-md">{aboutHead}</h3>
-                <p>{aboutText}</p>
-                <div className="footer-box-subgrid">
-                  <div className="contact-info">
-                    <img src={githubIcon} alt="github"/>
-                    <a href={github}>Github</a>
-                  </div>
-                  <div className="contact-info">
-                      <img src={linkedinIcon} alt="linkedin"/>
-                      <a href={linkedin}>Linkedin</a>
-                  </div>
-                  <div className="contact-info">
-                    <img src={phoneIcon} alt="phone"/>
-                    <span className="dark-spans">{phone}</span>
-                  </div>
+          <div className="footer-body-grid">
+            <div className="footer-box">
+              <h3 className="heading heading-md">{aboutHead}</h3>
+              <p>{aboutText}</p>
+              <div className="footer-box-subgrid">
+                <div className="contact-info">
+                  <img src={githubIcon} alt="github" />
+                  <a href={github}>Github</a>
                 </div>
-             </div>
-             <div className="footer-box">
-               <h3 className="heading heading-md">{contactHead}</h3>
-               <Formy />
-             </div>
-           </div>
+                <div className="contact-info">
+                  <img src={linkedinIcon} alt="linkedin" />
+                  <a href={linkedin}>Linkedin</a>
+                </div>
+                <div className="contact-info">
+                  <img src={phoneIcon} alt="phone" />
+                  <span className="dark-spans">{phone}</span>
+                </div>
+              </div>
+            </div>
+            <div className="footer-box">
+              <h3 className="heading heading-md no-mb">{contactHead}</h3>
+              <Formy />
+            </div>
+          </div>
         </div>
       </div>
-		</footer>
-		)
+    </footer>
+  )
 }
 
 export default Footer
--- src/components/form.js
+++ src/components/form.js
@@ -1,88 +1,87 @@
 import React from "react"
-import { withFormik, Form, Field } from "formik";
-import * as Yup from "yup";
+import * as Yup from "yup"
+import { withFormik, Form, Field } from "formik"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
-const Formy = ({ 
-  errors, 
-  touched, 
-  status,
-  isSubmitting,
-  handleSubmit 
-}) => {
-  console.log(status)
-  const isDirty = (errors.name || errors.email) || errors.message ? true : false
+const Formy = ({ errors, touched, isSubmitting, handleSubmit }) => {
+  const isDirty = errors.name || errors.email || errors.message ? true : false
+
   return (
-    <Form 
-    className="form"
-    name="contact"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
-    onSubmit={handleSubmit}
+    <Form
+      className="form"
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit}
     >
-      <div className="notification">{touched.name && errors.name && <p>{errors.name}</p>}</div>
-      <Field 
+      <div className="notification">
+        {touched.name && errors.name && <p>{errors.name}</p>}
+      </div>
+      <Field
         className="field"
-        type="text" 
-        name="name" 
-        placeholder="Full Name" 
+        type="text"
+        name="name"
+        placeholder="Full Name"
       />
-      <div className="notification">{touched.email && errors.email && <p>{errors.email}</p>}</div>
-      <Field 
-        className="field"
-        type="email" 
-        name="email"
-        placeholder="Email"  
-      />
+      <div className="notification">
+        {touched.email && errors.email && <p>{errors.email}</p>}
+      </div>
+      <Field className="field" type="email" name="email" placeholder="Email" />
       <div className="notification">
         {touched.message && errors.message && <p>{errors.message}</p>}
       </div>
-      <Field 
+      <Field
         className="field field-message"
-        component="textarea" 
-        rows="2" 
-        type="text" 
+        component="textarea"
+        rows="2"
+        type="text"
         name="message"
+        placeholder="Write your message here"
       />
-      <button disabled={isDirty || isSubmitting} type="submit">
-        { isSubmitting ? "loading..." : "Send"}
+      <ToastContainer />
+      <button
+        className="send-btn"
+        disabled={isDirty || isSubmitting}
+        type="submit"
+      >
+        {isSubmitting ? "loading..." : "Send"}
       </button>
     </Form>
-  );
-};
+  )
+}
 
 const mapPropsToValues = ({ name, email, message }) => {
   return {
     name: name || "",
     email: email || "",
-    message: message || ""
-  };
-};
+    message: message || "",
+  }
+}
+
+const success = () =>
+  toast("email succesfully sent", {
+    type: "success",
+    className: "toast-success",
+  })
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, "too short!")
-    .required("required"),
-  email: Yup.string()
-    .email("not a valid email")
-    .required("required"),
-  message: Yup.string().min(4, "too short!").required()
-});
+  name: Yup.string().min(2, "too short!").required("required"),
+  email: Yup.string().email("not a valid email").required("required"),
+  message: Yup.string().min(5, "too short!").required(),
+})
 
 const handleSubmit = (values, { resetForm, setSubmitting, setStatus }) => {
   setTimeout(() => {
-    if (values.message.length < 5 || values.message === "") {
-      console.log(values.message);
-    } else {
-      resetForm();
-      setStatus(false)
-      setSubmitting(false);
-    }
-  }, 1500);
-};
+    success()
+    resetForm()
+    setSubmitting(false)
+  }, 1500)
+}
 
 export default withFormik({
   mapPropsToValues,
   validationSchema,
-  handleSubmit
-})(Formy);
+  handleSubmit,
+})(Formy)
