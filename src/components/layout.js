e27782165ac55dc3e64f61f118e27ec27600ff08
rush2di <roch2di@gmail.com>
2020-05-29 18:01:05 +0000

    Include screeSpyProvider

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,39 @@
 import React from "react"
 
-import { ThemeContextProvider, LangContextProvider, useThemes } from "../utils/utils"
+import {
+  ThemeContextProvider,
+  LangContextProvider,
+  ScreenSpyProvider,
+  useThemes,
+} from "../utils/utils"
 import Header from "./header"
 import Footer from "./footer"
 import "../styles/styles.scss"
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <LangContextProvider>
       <ThemeContextProvider>
-	      <AppWrapper>      
-		     <main className="section">
-		     	{children}
-		     </main>
-		    </AppWrapper>
+        <ScreenSpyProvider>
+          <AppWrapper>
+            <main className="section">{children}</main>
+          </AppWrapper>
+        </ScreenSpyProvider>
       </ThemeContextProvider>
     </LangContextProvider>
   )
 }
 
-const AppWrapper = ({children}) => {
+const AppWrapper = ({ children }) => {
   const { backgroundStyle } = useThemes()
 
-	return (
-	  <div className={backgroundStyle}>
-	     <Header/>
-	     {children}
-	     <Footer />
-	  </div>
-	)
+  return (
+    <div className={backgroundStyle}>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  )
 }
 
 export default Layout
