fc368feb97c2536b2ba773233d722d5a8ba4a8af
rush2di <roch2di@gmail.com>
2020-05-29 18:02:49 +0000

    Reconfig screenSpy response

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,17 +1,22 @@
-import React, { createContext, useContext, useRef, useEffect, useState} from "react"
+import React, {
+  createContext,
+  useContext,
+  useRef,
+  useEffect,
+  useState,
+} from "react"
 
 import logoLight from "../assets/logo-light.svg"
 import logoDark from "../assets/logo-dark.svg"
 import moonIcon from "../assets/moon.svg"
 import sunIcon from "../assets/sun.svg"
 
-
 /*
  useScreenSpy hook checks the window screen width on first 
  mount and after every window resize event and returns it value
 */
 
-const ScreenSpy = createContext<number>(500)
+const ScreenSpy = createContext<Object>(500)
 
 export const ScreenSpyProvider = ({ children }) => {
   const [dimensions, setDimensions] = useState(0)
@@ -28,7 +33,9 @@ export const ScreenSpyProvider = ({ children }) => {
     }
   }, [])
 
-  return <ScreenSpy.Provider value={dimensions}>{children}</ScreenSpy.Provider>
+  return (
+    <ScreenSpy.Provider value={{ dimensions }}>{children}</ScreenSpy.Provider>
+  )
 }
 
 export const useScreenSpy = () => useContext(ScreenSpy)
@@ -39,33 +46,40 @@ of themeContextProvider
 */
 const themeContext = createContext(null)
 
-export const ThemeContextProvider = ({children}) => {
-	const [ isDark, setIsDark ] = useState<boolean>(false)
+export const ThemeContextProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState<boolean>(false)
 
-	const backgroundStyle = isDark ? "dark-bg" : "light-bg"
-	const textStyle = isDark ? "dark-spans" : "light-spans"
-	const logoStyle = isDark ? logoLight : logoDark
-	const iconStyle = isDark ? moonIcon : sunIcon
+  const backgroundStyle = isDark ? "dark-bg" : "light-bg"
+  const textStyle = isDark ? "dark-spans" : "light-spans"
+  const logoStyle = isDark ? logoLight : logoDark
+  const iconStyle = isDark ? moonIcon : sunIcon
 
-	const handleThemeToggle = ():void => (
-			isDark ? setIsDark(false) : setIsDark(true)
-		)
+  const handleThemeToggle = (): void =>
+    isDark ? setIsDark(false) : setIsDark(true)
 
-	return (
-		<themeContext.Provider value={{logoStyle, textStyle, backgroundStyle, iconStyle, handleThemeToggle}}>
-			{children}
-		</themeContext.Provider>
-		)
+  return (
+    <themeContext.Provider
+      value={{
+        logoStyle,
+        textStyle,
+        backgroundStyle,
+        iconStyle,
+        handleThemeToggle,
+      }}
+    >
+      {children}
+    </themeContext.Provider>
+  )
 }
 
 type themeConfig = {
-	paragraphStyle: string,
-	logoStyle: string,
-	iconStyle: string
-	handleThemeToggle: any
+  paragraphStyle: string
+  logoStyle: string
+  iconStyle: string
+  handleThemeToggle: any
 }
 
-export const useThemes = ():themeConfig  => useContext(themeContext)
+export const useThemes = (): themeConfig => useContext(themeContext)
 
 /*
 useThemeSet hook passes the theme configs to child elements 
@@ -73,34 +87,33 @@ of themeContextProvider
 */
 const langContext = createContext(null)
 
+export const LangContextProvider = ({ children }) => {
+  const [lang, setLang] = useState<string>("english")
+  const setEnglish = (): void => lang === "french" && setLang("english")
+  const setFrench = (): void => lang === "english" && setLang("french")
 
-export const LangContextProvider = ({children}) => {
-	const [ lang, setLang ] = useState<string>("english")
-	const setEnglish = ():void => lang === "french" && setLang("english")
-	const setFrench = ():void => lang === "english" && setLang("french")
-
-	return (
-		<langContext.Provider value={{lang, setEnglish, setFrench}}>
-			{children}
-		</langContext.Provider>
-		)
+  return (
+    <langContext.Provider value={{ lang, setEnglish, setFrench }}>
+      {children}
+    </langContext.Provider>
+  )
 }
 
 type langConfig = {
-	lang: string,
-	setEnglish: any,
-	setFrench: any
+  lang: string
+  setEnglish: any
+  setFrench: any
 }
 
-export const useLanguages = ():langConfig  => useContext(langContext)
+export const useLanguages = (): langConfig => useContext(langContext)
 
 /*
 function to transform query results to an array and returns 
 a filtred one depending on the selected language
 */
-export const languageFilter = (obj: object,lang: string):object => {
-	const toArray = Object.keys(obj).map((key) => ({[key]: obj[key]}))
-	const filtred = toArray.filter( val => Object.keys(val).includes(lang))
+export const languageFilter = (obj: object, lang: string): object => {
+  const toArray = Object.keys(obj).map(key => ({ [key]: obj[key] }))
+  const filtred = toArray.filter(val => Object.keys(val).includes(lang))
 
-	return filtred[0][lang]
+  return filtred[0][lang]
 }
