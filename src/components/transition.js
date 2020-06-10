2b60f2f786450982610730a9ef0e9d9844a1d210
rush2di <roch2di@gmail.com>
2020-06-10 18:51:33 +0000

    Init routes transition effects

---
+++ src/components/transition.js
@@ -0,0 +1,56 @@
+import React from "react"
+import {
+  TransitionGroup,
+  Transition as ReactTransition,
+} from "react-transition-group"
+import { useThemes } from "../utils/utils"
+
+const timeout = 500
+
+const getOpacityTransition = {
+  entering: {
+    position: `absolute`,
+    opacity: 0,
+    transform: "translateY(2rem)",
+  },
+  entered: {
+    transition: `all ${timeout}ms ease-in-out`,
+    opacity: 1,
+  },
+  exiting: {
+    transition: `all ${timeout}ms ease-in-out`,
+    opacity: 0,
+    transform: "translateY(4rem)",
+  },
+}
+
+const Transition = ({ children, location }) => {
+  const styleFixer = { position: "relative", width: "100%" }
+  const { backgroundStyle, coverStyle, logoStyle } = useThemes()
+
+  return (
+    <TransitionGroup>
+      <ReactTransition
+        key={!!location ? location.pathname : ""}
+        timeout={{
+          enter: timeout,
+          exit: timeout,
+        }}
+      >
+        {status => (
+          <div style={styleFixer} className={backgroundStyle}>
+            <div
+              style={{
+                ...getOpacityTransition[status],
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        )}
+      </ReactTransition>
+    </TransitionGroup>
+  )
+}
+
+export default Transition
