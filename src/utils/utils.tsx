e312ae315fc81e5215e97d141acc60ba3c89e43c
rush2di <roch2di@gmail.com>
2020-06-20 01:05:58 +0100

    Set new helpers

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -10,10 +10,10 @@ import sunIcon from "../assets/sun.svg"
 helper function uses scrollmagic to trigger the right animatiom
 passed to it as an argument and accordingly to it's configs
 */
-export const triggerAnimation = () => {
+export const triggerAnimation = (): object => {
   const controller = new ScrollMagic.Controller()
 
-  const scene = (trigger, offset, callback) => {
+  const scene = (trigger: string, offset: number, callback: any) => {
     new ScrollMagic.Scene({
       triggerElement: trigger,
       duration: 0,
@@ -26,6 +26,25 @@ export const triggerAnimation = () => {
 
   return { scene }
 }
+
+// helper function to slice the hero title and trim it on mobile view
+export const titleFixer = (
+  dimensions: number,
+  title: string
+): Array<string> => {
+  const firstHeading = title.substring(0, title.search("Ro"))
+  const secondHeading = title.substring(title.search("Ro"))
+
+  const firstHeadingMobile =
+    dimensions <= 425 ? title.substring(0, title.search(",") + 1) : firstHeading
+  const secondHeadingMobile =
+    dimensions <= 425
+      ? title.substring(title.search(",") + 1, title.search("Be"))
+      : secondHeading
+
+  return [firstHeadingMobile, secondHeadingMobile]
+}
+
 /*
  useScreenSpy hook checks the window screen width on first 
  mount and after every window resize event and returns it value
